b8b12519a223df92a7f62c3916a081aee18a83e0
Nicholas Powers <njpowers7915@gmail.com>
2018-08-05 16:44:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,9 +28,6 @@ class Passenger {
     this.name = name
     store.passengers.push(this)
   }
-  setDriver(driver) {
-    this.driverId = driver.id
-  }
 
   trips(){
     return store.trips.filter(trip => {
